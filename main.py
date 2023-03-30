19585a2e924c0e9db573ec463e26a60117b1045d
ABeack24 <818639@mystma.org>
2023-03-30 14:21:38 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA(x):
+    numA = 0
+    for i in range (0, len(x)):
+        if x[i] == "a":
+            numA = numA + 1
+    return numA        
+
+print(countA("caat"))
